9b3ab71b93ea16b6ea52508666fa7134fd326eab
Harshroxnox <harshroxnox@gmail.com>
2024-09-15 05:36:46 +0530

    using cors to send token

--- main-pdf-frontend/src/App.jsx
+++ main-pdf-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Answer from "./answer.jsx";
 import Header from "./header.jsx";
@@ -8,6 +8,29 @@ import History from "./history.jsx";
 const Home = () => <div><Answer /></div>;
 
 const App = () => {
+
+  useEffect(() => {
+    // Listener to receive messages from other ports
+    const handleMessage = (event) => {
+      // Verify the origin of the message
+     // if (event.origin !== "http://localhost:5500") return;
+
+      const { key, value } = event.data;
+      console.log(event.data)
+      // Store data in localStorage or handle it as needed
+      localStorage.setItem(key, value);
+      console.log(`Received data: ${key} = ${value}`);
+    };
+
+    // Add the event listener for message events
+    window.addEventListener("message", handleMessage);
+
+    // Clean up the event listener when component unmounts
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
   const [history, setHistory] = useState([
     { date_posted: "14-09-2024", db_pdf_name: "58ca1b05d74a7224.pdf", pdf_id: 3, pdf_name: "SIH.pdf" },
     { date_posted: "14-09-2024", db_pdf_name: "9e2be9e771d1e5ef.pdf", pdf_id: 4, pdf_name: "Gr. 12.pdf" }
